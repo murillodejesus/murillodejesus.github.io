40790047ce0435bd1f94dd389940ebfe63bfc664
murillodejesus <murillodejesus02@gmail.com>
2025-03-19 07:32:04 -0300

    update

--- wdd230/scripts/getdates.js
+++ wdd230/scripts/getdates.js
@@ -1,16 +1,2 @@
-// Dynamically populate the current year
-const yearElement = document.getElementById('currentyear');
-const currentYear = new Date().getFullYear();
-yearElement.textContent = currentYear;
-
-// Dynamically populate the last modified date
-const lastModifiedElement = document.getElementById('lastModified');
-lastModifiedElement.textContent = 'Last Modification: ' + document.lastModified;
-
-document.getElementById("menu-toggle").addEventListener("click", function () {
-    document.getElementById("menu").classList.toggle("active");
-});
-
-document.getElementById("dark-mode-toggle").addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
-});
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
